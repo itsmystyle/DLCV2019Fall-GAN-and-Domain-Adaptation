da8e046c5dc74fdb6d2b5b303ed4483061fed0e7
YwuanChai <itsmystyle.0821@gmail.com>
2019-11-19 23:13:53 +0800

    added tensorboard, set seed, trainable

--- module/dcgan/trainer.py
+++ module/dcgan/trainer.py
@@ -1,4 +1,5 @@
 import os
+import argparse
 
 import torch
 import torch.nn as nn
@@ -8,10 +9,12 @@ import torchvision.utils as vutils
 from tqdm import tqdm
 from torch import optim
 from torch.utils.data import DataLoader
+from tensorboardX import SummaryWriter
 
 from module.dcgan.generator import Generator
 from module.dcgan.discriminator import Discriminator
-from module.utils import weights_init
+from module.utils import weights_init, set_random_seed
+from module.dataset.celebA import CelebADataset
 
 
 class Trainer:
@@ -34,6 +37,8 @@ class Trainer:
         # Models
         self.generator = Generator()
         self.discriminator = Discriminator()
+        self.generator.to(self.device)
+        self.discriminator.to(self.device)
         self.generator.apply(weights_init)
         self.discriminator.apply(weights_init)
         print(self.generator)
@@ -75,7 +80,7 @@ class Trainer:
             self._eval_one_epoch(epoch)
 
             # save model
-            self.save(self.save_dir, epoch)
+            self.save(epoch)
 
     def _run_one_epoch(self, epoch, iters):
         # train generator and discriminator one epoch
@@ -89,21 +94,20 @@ class Trainer:
         )
 
         for idx, (images, labels) in trange:
+            bs = images.shape[0]
 
             ###############################
             # Train D with real image
             self.discriminator.zero_grad()
             real_image = images.to(self.device)
-            label = torch.full((self.batch_size,), self.real_label, device=self.device)
+            label = torch.full((bs,), self.real_label, device=self.device)
             output = self.discriminator(real_image)
             errD_real = self.criterion(output.view(-1), label)
             errD_real.backward()
             D_x = output.mean().item()
 
             # Train D with fake image
-            noise = torch.randn(
-                self.batch_size, self.latent_dim, 1, 1, device=self.device
-            )
+            noise = torch.randn(bs, self.latent_dim, 1, 1, device=self.device)
             fake = self.generator(noise)
             label.fill_(self.fake_label)
             output = self.discriminator(fake.detach())
@@ -131,13 +135,13 @@ class Trainer:
 
             # update tqdm
             postfix_dict = {
-                "dRerr": "{:.3f}".format(errD_real.item()),
-                "dFerr": "{:.3f}".format(errD_fake.item()),
-                "dErr": "{:.3f}".format(errD.item()),
-                "gErr": "{:.3f}".format(errG.item()),
-                "dRm": "{:.3f}".format(D_x),
-                "dFm": "{:.3f}".format(D_G_z1),
-                "gFm": "{:.3f}".format(D_G_z2),
+                "errR": "{:.3f}".format(errD_real.item()),
+                "errF": "{:.3f}".format(errD_fake.item()),
+                "mDe": "{:.3f}".format(errD.item()),
+                "mGe": "{:.3f}".format(errG.item()),
+                "odRm": "{:.3f}".format(D_x),
+                "odFm": "{:.3f}".format(D_G_z1),
+                "ogFm": "{:.3f}".format(D_G_z2),
             }
             trange.set_postfix(**postfix_dict)
 
@@ -163,13 +167,16 @@ class Trainer:
 
         plt.figure(figsize=(8, 8))
         plt.axis("off")
-        plt.title("Training Images")
+        plt.title("Fixed Noise Images")
         plt.imshow(
             np.transpose(
                 vutils.make_grid(fake, padding=2, normalize=True).cpu(), (1, 2, 0),
             ),
         )
-        plt.savefig(os.path.join(self.save_dir, "epoch_{}.png".format(epoch)))
+        dir = os.path.join(self.save_dir, "figures")
+        if not os.path.exists(dir):
+            os.makedirs(dir)
+        plt.savefig(os.path.join(dir, "epoch_{}.png".format(epoch)))
 
     def save(self, epoch):
         torch.save(
@@ -180,3 +187,33 @@ class Trainer:
             self.discriminator.state_dict(),
             os.path.join(self.save_dir, "discriminator_{}.pth.tar".format(epoch)),
         )
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description="DCGan trainer.")
+
+    parser.add_argument("--epochs", type=int, default=5, help="Training epochs.")
+    parser.add_argument("model_dir", type=str, help="Directory path to store models.")
+    parser.add_argument("attribute_path", type=str, help="Path to load attributes csv.")
+    parser.add_argument("images_dir", type=str, help="Path to images stored directory.")
+    parser.add_argument("--random_seed", type=int, default=42, help="random seed.")
+
+    args = parser.parse_args()
+
+    set_random_seed(args.random_seed)
+    dataset = CelebADataset(args.attribute_path, args.images_dir)
+    writer = SummaryWriter(os.path.join(args.model_dir, "train_logs"))
+
+    trainer = Trainer(
+        args.epochs,
+        dataset,
+        writer,
+        args.model_dir,
+        lr=2e-4,
+        beta1=0.5,
+        workers=4,
+        batch_size=128,
+    )
+
+    trainer.fit()
