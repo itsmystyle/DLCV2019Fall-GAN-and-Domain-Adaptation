dcc354607cf627c42809c130dd3975d134f92a47
YwuanChai <itsmystyle.0821@gmail.com>
2019-11-19 21:12:58 +0800

    added set seed and init weights

---
+++ module/utils.py
@@ -0,0 +1,21 @@
+import random
+
+import torch
+import torch.nn as nn
+import numpy as np
+
+
+def set_random_seed(seed):
+    print("Random seed:", seed)
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+
+
+def weights_init(m):
+    classname = m.__class__.__name__
+    if classname.find("Conv") != -1:
+        nn.init.normal_(m.weight.data, 0.0, 0.02)
+    elif classname.find("BatchNorm") != -1:
+        nn.init.normal_(m.weight.data, 1.0, 0.02)
+        nn.init.constant_(m.bias.data, 0)
