44b68caa7baab967b2272d0f0c632db40a9a849d
YwuanChai <itsmystyle.0821@gmail.com>
2019-11-20 15:00:21 +0800

    added acgan generator

---
+++ module/acgan/generator.py
@@ -0,0 +1,43 @@
+import torch
+import torch.nn as nn
+
+
+class Generator(nn.Module):
+    def __init__(self, latent_dim=100, n_feature_maps=64, n_class=2, embedding_dim=20):
+        super(Generator, self).__init__()
+
+        self.latent_dim = latent_dim
+        self.n_feature_maps = n_feature_maps
+        self.n_class = n_class
+        self.embedding_dim = embedding_dim
+
+        self.class_embedding = nn.Embedding(self.n_class, self.embedding_dim)
+
+        self.conv_blocks = nn.Sequential(
+            nn.ConvTranspose2d(
+                (self.latent_dim + self.embedding_dim), self.n_feature_maps * 8, 4, 1, 0, bias=False
+            ),
+            nn.BatchNorm2d(self.n_feature_maps * 8),
+            nn.ReLU(True),
+            nn.ConvTranspose2d(
+                self.n_feature_maps * 8, self.n_feature_maps * 4, 4, 2, 1, bias=False
+            ),
+            nn.BatchNorm2d(self.n_feature_maps * 4),
+            nn.ReLU(True),
+            nn.ConvTranspose2d(
+                self.n_feature_maps * 4, self.n_feature_maps * 2, 4, 2, 1, bias=False
+            ),
+            nn.BatchNorm2d(self.n_feature_maps * 2),
+            nn.ReLU(True),
+            nn.ConvTranspose2d(self.n_feature_maps * 2, self.n_feature_maps, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(self.n_feature_maps),
+            nn.ReLU(True),
+            nn.ConvTranspose2d(self.n_feature_maps, 3, 4, 2, 1, bias=False),
+            nn.Tanh(),
+        )
+
+    def forward(self, X, labels):
+        label_embeds = self.class_embedding(labels)
+        label_embeds = label_embeds.unsqueeze(-1).unsqueeze(-1)
+        X = torch.cat((X, label_embeds), dim=1)
+        return self.conv_blocks(X)
