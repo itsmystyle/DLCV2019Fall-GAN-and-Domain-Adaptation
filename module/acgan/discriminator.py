39fc1eddb3b2a3c98e2d68fec04170b275a939d2
YwuanChai <itsmystyle.0821@gmail.com>
2019-11-22 23:28:11 +0800

    added batchnorm and dropout to D

--- module/acgan/discriminator.py
+++ module/acgan/discriminator.py
@@ -3,7 +3,7 @@ import torch.nn.utils as U
 
 
 class Discriminator(nn.Module):
-    def __init__(self, n_feature_maps=128, n_class=1, n_attributes=7, p=0.25):
+    def __init__(self, n_feature_maps=64, n_class=1, n_attributes=7, p=0.1):
         super(Discriminator, self).__init__()
 
         self.n_feature_maps = n_feature_maps
@@ -12,6 +12,7 @@ class Discriminator(nn.Module):
         self.p = p
         self.conv_blocks = nn.Sequential(
             U.spectral_norm(nn.Conv2d(3, self.n_feature_maps, 4, 2, 1, bias=False)),
+            nn.BatchNorm2d(self.n_feature_maps),
             nn.LeakyReLU(0.2, inplace=True),
             U.spectral_norm(
                 nn.Conv2d(
@@ -20,7 +21,7 @@ class Discriminator(nn.Module):
             ),
             nn.BatchNorm2d(self.n_feature_maps * 2),
             nn.LeakyReLU(0.2, inplace=True),
-            # nn.Dropout2d(self.p),
+            nn.Dropout2d(self.p),
             U.spectral_norm(
                 nn.Conv2d(
                     self.n_feature_maps * 2,
@@ -33,7 +34,7 @@ class Discriminator(nn.Module):
             ),
             nn.BatchNorm2d(self.n_feature_maps * 4),
             nn.LeakyReLU(0.2, inplace=True),
-            # nn.Dropout2d(self.p),
+            nn.Dropout2d(self.p),
             U.spectral_norm(
                 nn.Conv2d(
                     self.n_feature_maps * 4,
@@ -46,7 +47,7 @@ class Discriminator(nn.Module):
             ),
             nn.BatchNorm2d(self.n_feature_maps * 8),
             nn.LeakyReLU(0.2, inplace=True),
-            # nn.Dropout2d(self.p),
+            nn.Dropout2d(self.p),
             U.spectral_norm(
                 nn.Conv2d(
                     self.n_feature_maps * 8, self.n_attributes + 1, 4, 1, 0, bias=False
@@ -59,5 +60,5 @@ class Discriminator(nn.Module):
         X = self.conv_blocks(X)
         X = X.squeeze()
 
-        # return adv_loss, aux_loss, smile_loss
-        return X[:, 0], X[:, 1:-1], X[:, -1]
+        # return adv_loss, smile_loss
+        return X[:, 0], X[:, 1:]
