612c1d247a99078449bc0445541329fdc348d77f
YwuanChai <itsmystyle.0821@gmail.com>
2019-11-23 18:36:06 +0800

    added able to load multiple domain images at once

--- module/dataset/digit.py
+++ module/dataset/digit.py
@@ -14,10 +14,16 @@ from module.dataset.utils import get_transform
 
 class DigitDataset(Dataset):
     def __init__(
-        self, label_path, images_dir, transform=None,
+        self, label_paths, images_dirs, transform=None,
     ):
-        self.images_dir = images_dir
-        self.data = pd.read_csv(label_path)
+        self.images_dirs = images_dirs
+        self.multiple_domain = True if len(images_dirs) > 1 else False
+        datas = []
+        for idx, label_path in enumerate(label_paths):
+            data = pd.read_csv(label_path)
+            data["domain"] = idx
+            datas.append(data)
+        self.data = pd.concat(datas)
 
         if transform:
             self.transform = transform
@@ -28,28 +34,37 @@ class DigitDataset(Dataset):
         return self.data.shape[0]
 
     def __getitem__(self, index):
-        image_path = os.path.join(self.images_dir, self.data.iloc[index].image_name)
+        domain = self.data.iloc[index].domain
+        image_path = os.path.join(
+            self.images_dirs[domain], self.data.iloc[index].image_name
+        )
         image = Image.open(image_path)
         image = self.transform(image)
         label = self.data.iloc[index].label
-        label = torch.tensor(label).long()
 
-        return (image, label)
+        return (image, torch.tensor(label).long(), torch.tensor(domain).long())
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Prepare MNIST-M dataset.")
 
-    parser.add_argument("label_path", type=str, help="Path to load label csv.")
-    parser.add_argument("images_dir", type=str, help="Path to images stored directory.")
+    parser.add_argument(
+        "--label_paths", nargs="+", help="Paths to load label csv.", required=True
+    )
+    parser.add_argument(
+        "--images_dirs",
+        nargs="+",
+        help="Paths to images stored directory.",
+        required=True,
+    )
 
     args = parser.parse_args()
 
-    dataset = DigitDataset(args.label_path, args.images_dir)
-    dataloader = DataLoader(dataset, batch_size=64, shuffle=False)
-    images, labels = next(iter(dataloader))
+    dataset = DigitDataset(args.label_paths, args.images_dirs)
+    dataloader = DataLoader(dataset, batch_size=64, shuffle=True)
+    images, labels, domains = next(iter(dataloader))
 
-    print(images.shape, labels.shape)
+    print(images.shape, labels.shape, domains.shape)
 
     plt.figure(figsize=(8, 8))
     plt.axis("off")
