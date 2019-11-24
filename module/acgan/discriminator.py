013f007ffaf882ae0e87894b7e9fcafe18473fd9
YwuanChai <itsmystyle.0821@gmail.com>
2019-11-24 20:43:30 +0800

    added bias removed bn and dropout

--- module/acgan/discriminator.py
+++ module/acgan/discriminator.py
@@ -11,47 +11,29 @@ class Discriminator(nn.Module):
         self.n_class = n_class
         self.p = p
         self.conv_blocks = nn.Sequential(
-            U.spectral_norm(nn.Conv2d(3, self.n_feature_maps, 4, 2, 1, bias=False)),
-            nn.BatchNorm2d(self.n_feature_maps),
-            nn.LeakyReLU(0.2, inplace=True),
+            U.spectral_norm(nn.Conv2d(3, self.n_feature_maps, 4, 2, 1)),
+            # nn.BatchNorm2d(self.n_feature_maps),
+            nn.LeakyReLU(0.02, inplace=True),
             U.spectral_norm(
-                nn.Conv2d(
-                    self.n_feature_maps, self.n_feature_maps * 2, 4, 2, 1, bias=False
-                )
+                nn.Conv2d(self.n_feature_maps, self.n_feature_maps * 2, 4, 2, 1)
             ),
-            nn.BatchNorm2d(self.n_feature_maps * 2),
-            nn.LeakyReLU(0.2, inplace=True),
-            nn.Dropout2d(self.p),
+            # nn.BatchNorm2d(self.n_feature_maps * 2),
+            nn.LeakyReLU(0.02, inplace=True),
+            # nn.Dropout2d(self.p),
             U.spectral_norm(
-                nn.Conv2d(
-                    self.n_feature_maps * 2,
-                    self.n_feature_maps * 4,
-                    4,
-                    2,
-                    1,
-                    bias=False,
-                )
+                nn.Conv2d(self.n_feature_maps * 2, self.n_feature_maps * 4, 4, 2, 1)
             ),
-            nn.BatchNorm2d(self.n_feature_maps * 4),
-            nn.LeakyReLU(0.2, inplace=True),
-            nn.Dropout2d(self.p),
+            # nn.BatchNorm2d(self.n_feature_maps * 4),
+            nn.LeakyReLU(0.02, inplace=True),
+            # nn.Dropout2d(self.p),
             U.spectral_norm(
-                nn.Conv2d(
-                    self.n_feature_maps * 4,
-                    self.n_feature_maps * 8,
-                    4,
-                    2,
-                    1,
-                    bias=False,
-                )
+                nn.Conv2d(self.n_feature_maps * 4, self.n_feature_maps * 8, 4, 2, 1)
             ),
-            nn.BatchNorm2d(self.n_feature_maps * 8),
-            nn.LeakyReLU(0.2, inplace=True),
-            nn.Dropout2d(self.p),
+            # nn.BatchNorm2d(self.n_feature_maps * 8),
+            nn.LeakyReLU(0.02, inplace=True),
+            # nn.Dropout2d(self.p),
             U.spectral_norm(
-                nn.Conv2d(
-                    self.n_feature_maps * 8, self.n_attributes + 1, 4, 1, 0, bias=False
-                )
+                nn.Conv2d(self.n_feature_maps * 8, self.n_attributes + 1, 4, 1, 0)
             ),
             nn.Sigmoid(),
         )
