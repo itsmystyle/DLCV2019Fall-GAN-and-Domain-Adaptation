b5cd63f77c90bc9ffedc1821612a4159eebbb940
YwuanChai <itsmystyle.0821@gmail.com>
2019-11-26 20:50:21 +0800

    added multiclass accuracy

--- module/metrics.py
+++ module/metrics.py
@@ -1,3 +1,6 @@
+import torch
+
+
 class Metrics:
     def __init__(self):
         self.name = "Metric Name"
@@ -28,6 +31,29 @@ class BCAccuracy(Metrics):
 
     def update(self, predicts, targets):
         self.n_correct += ((predicts > 0.5) == targets).sum()
+        self.n += targets.shape[0]
+
+    def get_score(self):
+        return self.n_correct / self.n
+
+
+class MulticlassAccuracy(Metrics):
+    """ Multiclass Classification Accuracy
+    """
+
+    def __init__(self, threshold=0.5):
+        self.name = "Acc."
+        self.threshold = threshold
+        self.n_correct = 0
+        self.n = 1e-20
+
+    def reset(self):
+        self.n_correct = 0
+        self.n = 1e-20
+
+    def update(self, predicts, targets):
+        predicts = torch.exp(predicts).max(dim=1)[1]
+        self.n_correct += (predicts == targets).sum().item()
         self.n += targets.shape[0]
 
     def get_score(self):
