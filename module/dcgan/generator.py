17c5a0533edda90415e3cebfc2efef855c27b11c
YwuanChai <itsmystyle.0821@gmail.com>
2019-11-26 13:44:27 +0800

    added spectral norm

--- module/dcgan/generator.py
+++ module/dcgan/generator.py
@@ -1,4 +1,5 @@
 import torch.nn as nn
+import torch.nn.utils as U
 
 
 class Generator(nn.Module):
@@ -9,23 +10,47 @@ class Generator(nn.Module):
         self.n_feature_maps = n_feature_maps
 
         self.conv_blocks = nn.Sequential(
-            nn.ConvTranspose2d(self.latent_dim, self.n_feature_maps * 8, 4, 1, 0, bias=False),
+            U.spectral_norm(
+                nn.ConvTranspose2d(
+                    self.latent_dim, self.n_feature_maps * 8, 4, 1, 0, bias=False
+                )
+            ),
             nn.BatchNorm2d(self.n_feature_maps * 8),
             nn.ReLU(True),
-            nn.ConvTranspose2d(
-                self.n_feature_maps * 8, self.n_feature_maps * 4, 4, 2, 1, bias=False
+            U.spectral_norm(
+                nn.ConvTranspose2d(
+                    self.n_feature_maps * 8,
+                    self.n_feature_maps * 4,
+                    4,
+                    2,
+                    1,
+                    bias=False,
+                )
             ),
             nn.BatchNorm2d(self.n_feature_maps * 4),
             nn.ReLU(True),
-            nn.ConvTranspose2d(
-                self.n_feature_maps * 4, self.n_feature_maps * 2, 4, 2, 1, bias=False
+            U.spectral_norm(
+                nn.ConvTranspose2d(
+                    self.n_feature_maps * 4,
+                    self.n_feature_maps * 2,
+                    4,
+                    2,
+                    1,
+                    bias=False,
+                )
             ),
             nn.BatchNorm2d(self.n_feature_maps * 2),
             nn.ReLU(True),
-            nn.ConvTranspose2d(self.n_feature_maps * 2, self.n_feature_maps, 4, 2, 1, bias=False),
+            U.spectral_norm(
+                nn.ConvTranspose2d(
+                    self.n_feature_maps * 2, self.n_feature_maps, 4, 2, 1, bias=False
+                )
+            ),
             nn.BatchNorm2d(self.n_feature_maps),
             nn.ReLU(True),
-            nn.ConvTranspose2d(self.n_feature_maps, 3, 4, 2, 1, bias=False),
+            U.spectral_norm(
+                nn.ConvTranspose2d(self.n_feature_maps, 3, 4, 2, 1, bias=False)
+            ),
             nn.Tanh(),
         )
 
