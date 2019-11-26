a32237d24964ef20960497f2511ccfb54fc517da
YwuanChai <itsmystyle.0821@gmail.com>
2019-11-26 13:44:14 +0800

    added spectral norm and bias, removed bn

--- module/dcgan/discriminator.py
+++ module/dcgan/discriminator.py
@@ -1,4 +1,5 @@
 import torch.nn as nn
+import torch.nn.utils as U
 
 
 class Discriminator(nn.Module):
@@ -7,24 +8,24 @@ class Discriminator(nn.Module):
 
         self.n_feature_maps = n_feature_maps
         self.conv_blocks = nn.Sequential(
-            nn.Conv2d(3, self.n_feature_maps, 4, 2, 1, bias=False),
-            nn.LeakyReLU(0.2, inplace=True),
-            nn.Conv2d(
-                self.n_feature_maps, self.n_feature_maps * 2, 4, 2, 1, bias=False
+            U.spectral_norm(nn.Conv2d(3, self.n_feature_maps, 4, 2, 1)),
+            nn.LeakyReLU(0.02, inplace=True),
+            U.spectral_norm(
+                nn.Conv2d(self.n_feature_maps, self.n_feature_maps * 2, 4, 2, 1)
             ),
-            nn.BatchNorm2d(self.n_feature_maps * 2),
-            nn.LeakyReLU(0.2, inplace=True),
-            nn.Conv2d(
-                self.n_feature_maps * 2, self.n_feature_maps * 4, 4, 2, 1, bias=False
+            # nn.BatchNorm2d(self.n_feature_maps * 2),
+            nn.LeakyReLU(0.02, inplace=True),
+            U.spectral_norm(
+                nn.Conv2d(self.n_feature_maps * 2, self.n_feature_maps * 4, 4, 2, 1)
             ),
-            nn.BatchNorm2d(self.n_feature_maps * 4),
-            nn.LeakyReLU(0.2, inplace=True),
-            nn.Conv2d(
-                self.n_feature_maps * 4, self.n_feature_maps * 8, 4, 2, 1, bias=False
+            # nn.BatchNorm2d(self.n_feature_maps * 4),
+            nn.LeakyReLU(0.02, inplace=True),
+            U.spectral_norm(
+                nn.Conv2d(self.n_feature_maps * 4, self.n_feature_maps * 8, 4, 2, 1)
             ),
-            nn.BatchNorm2d(self.n_feature_maps * 8),
-            nn.LeakyReLU(0.2, inplace=True),
-            nn.Conv2d(self.n_feature_maps * 8, 1, 4, 1, 0, bias=False),
+            # nn.BatchNorm2d(self.n_feature_maps * 8),
+            nn.LeakyReLU(0.02, inplace=True),
+            U.spectral_norm(nn.Conv2d(self.n_feature_maps * 8, 1, 4, 1, 0)),
             nn.Sigmoid(),
         )
 
