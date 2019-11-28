5cceeebe79445cef2a4ee3fc7a434a242b586715
YwuanChai <itsmystyle.0821@gmail.com>
2019-11-28 22:09:24 +0800

    removed batchnorm

--- module/da/dann.py
+++ module/da/dann.py
@@ -4,7 +4,7 @@ from module.da.utils import GradientReversalLayer
 
 
 class DANN(nn.Module):
-    def __init__(self, n_features=64, p=0.1):
+    def __init__(self, n_features=32, p=0.1):
         super(DANN, self).__init__()
 
         self.n_features = n_features
@@ -12,31 +12,31 @@ class DANN(nn.Module):
 
         self.feature_extractor = nn.Sequential(
             nn.Conv2d(3, self.n_features, 5),
-            nn.BatchNorm2d(64),
+            # nn.BatchNorm2d(self.n_features),
             nn.MaxPool2d(2),
             nn.ReLU(True),
-            nn.Conv2d(64, 50, 5),
-            nn.BatchNorm2d(50),
+            nn.Conv2d(self.n_features, self.n_features * 2, 5),
+            # nn.BatchNorm2d(self.n_features * 2),
             nn.Dropout2d(self.p),
             nn.MaxPool2d(2),
             nn.ReLU(True),
         )
 
         self.label_predictor = nn.Sequential(
-            nn.Linear(50 * 4 * 4, 100),
-            nn.BatchNorm1d(100),
+            nn.Linear(self.n_features * 2 * 4 * 4, 100),
+            # nn.BatchNorm1d(100),
             nn.ReLU(True),
             nn.Dropout(self.p),
             nn.Linear(100, 100),
-            nn.BatchNorm1d(100),
+            # nn.BatchNorm1d(100),
             nn.ReLU(True),
             nn.Linear(100, 10),
             nn.LogSoftmax(dim=1),
         )
 
         self.domain_classifier = nn.Sequential(
-            nn.Linear(50 * 4 * 4, 100),
-            nn.BatchNorm1d(100),
+            nn.Linear(self.n_features * 2 * 4 * 4, 100),
+            # nn.BatchNorm1d(100),
             nn.ReLU(True),
             nn.Linear(100, 1),
             nn.Sigmoid(),
@@ -44,7 +44,7 @@ class DANN(nn.Module):
 
     def forward(self, X, Lambda):
         features = self.feature_extractor(X)
-        features = features.view(-1, 50 * 4 * 4)
+        features = features.view(-1, self.n_features * 2 * 4 * 4)
 
         # label classification
         label_output = self.label_predictor(features)
@@ -57,6 +57,6 @@ class DANN(nn.Module):
 
     def extract_feature(self, X):
         features = self.feature_extractor(X)
-        features = features.view(-1, 50 * 4 * 4)
+        features = features.view(-1, self.n_features * 2 * 4 * 4)
 
         return features
