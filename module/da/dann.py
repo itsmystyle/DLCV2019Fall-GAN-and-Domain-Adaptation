fb5734c1a624e8f58adaf8d9fb86b850042b3cf1
YwuanChai <itsmystyle.0821@gmail.com>
2019-11-30 00:04:25 +0800

    changed dim from 100 to 1024 same as paper

--- module/da/dann.py
+++ module/da/dann.py
@@ -6,7 +6,7 @@ from module.dsn.utils import Flatten
 
 
 class DANN(nn.Module):
-    def __init__(self, n_features=32, p=0.1):
+    def __init__(self, n_features=64, p=0.1):
         super(DANN, self).__init__()
 
         self.n_features = n_features
@@ -20,30 +20,30 @@ class DANN(nn.Module):
             nn.ReLU(True),
             nn.Conv2d(self.n_features, self.n_features * 2, 5),
             nn.BatchNorm2d(self.n_features * 2),
-            nn.Dropout2d(self.p),
             nn.MaxPool2d(2),
             nn.ReLU(True),
+            nn.Dropout2d(self.p),
             Flatten(),
             nn.Linear(self.n_features * 2 * 4 * 4, self.latent_dim),
         )
 
         self.label_predictor = nn.Sequential(
-            nn.Linear(self.latent_dim, 100),
-            nn.BatchNorm1d(100),
+            nn.Linear(self.latent_dim, 1024),
+            nn.BatchNorm1d(1024),
             nn.ReLU(True),
             nn.Dropout(self.p),
-            nn.Linear(100, 100),
-            nn.BatchNorm1d(100),
+            nn.Linear(1024, 1024),
+            nn.BatchNorm1d(1024),
             nn.ReLU(True),
-            nn.Linear(100, 10),
+            nn.Linear(1024, 10),
             nn.LogSoftmax(dim=1),
         )
 
         self.domain_classifier = nn.Sequential(
-            nn.Linear(self.latent_dim, 100),
-            nn.BatchNorm1d(100),
+            nn.Linear(self.latent_dim, 1024),
+            nn.BatchNorm1d(1024),
             nn.ReLU(True),
-            nn.Linear(100, 1),
+            nn.Linear(1024, 1),
             nn.Sigmoid(),
         )
 
