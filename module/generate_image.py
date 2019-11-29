91a88c71cd6a887419f1174b4814b03d22e9a230
YwuanChai <itsmystyle.0821@gmail.com>
2019-11-29 21:31:51 +0800

    added load model and cherry pick noises

--- module/generate_image.py
+++ module/generate_image.py
@@ -17,24 +17,35 @@ if __name__ == "__main__":
     )
     parser.add_argument("--dcgan_model", type=str, help="Path to DCGan model.")
     parser.add_argument("--acgan_model", type=str, help="Path to ACGan model.")
-    parser.add_argument("--random_seed", type=int, default=42, help="random seed.")
+    parser.add_argument("--random_seed", nargs="+", default=None, help="random seed.")
     parser.add_argument("--batch_size", type=int, default=32, help="batch size.")
 
     args = parser.parse_args()
 
-    set_random_seed(args.random_seed)
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
     fig_1_path = os.path.join(args.output_dir, "fig1_2.jpg")
     fig_2_path = os.path.join(args.output_dir, "fig2_2.jpg")
 
     dcgen = DCGen(latent_dim=128)
+    dcgen.load_state_dict(torch.load(args.dcgan_model))
     dcgen.to(device)
     acgen = ACGen(latent_dim=120, n_feature_maps=128, embedding_dim=4, n_attributes=1)
+    acgen.load_state_dict(torch.load(args.acgan_model))
     acgen.to(device)
 
-    dc_noise = torch.randn(32, 128, 1, 1, device=device)
-    ac_noise = torch.randn(20, 120, 1, 1, device=device)
+    # prepare dc noise
+    set_random_seed(int(args.random_seed[0]))
+    dc_noise = torch.randn(64, 128, 1, 1, device=device)
+    dc_noise[1], dc_noise[2], dc_noise[5] = dc_noise[42], dc_noise[50], dc_noise[54]
+    dc_noise[9], dc_noise[18], dc_noise[28] = dc_noise[60], dc_noise[32], dc_noise[34]
+    dc_noise[27] = dc_noise[63]
+    dc_noise = dc_noise[:32]
+
+    # prepare ac noise
+    set_random_seed(int(args.random_seed[1]))
+    tmp_noise = torch.randn(28, 120, 1, 1, device=device)
+    ac_noise = torch.cat((tmp_noise[4:-8], tmp_noise[-4:]))
     ac_attribute = torch.randint(
         low=0, high=2, size=(20, 1), dtype=torch.long, device=device,
     )
@@ -53,12 +64,11 @@ if __name__ == "__main__":
     with torch.no_grad():
         fake = dcgen(dc_noise).detach().cpu()
 
-    plt.figure(figsize=(8, 4))
+    plt.figure(figsize=(8, 8))
     plt.axis("off")
     plt.imshow(
         np.transpose(
-            vutils.make_grid(fake, padding=2, normalize=True, scale_each=True).cpu(),
-            (1, 2, 0),
+            vutils.make_grid(fake, nrow=8, padding=2, normalize=True).cpu(), (1, 2, 0),
         ),
     )
     plt.savefig(fig_1_path)
@@ -70,11 +80,13 @@ if __name__ == "__main__":
     with torch.no_grad():
         fake = acgen(ac_noise, ac_attribute).detach().cpu()
 
-    plt.figure(figsize=(5, 2))
+    plt.figure(figsize=(8, 8))
     plt.axis("off")
     plt.imshow(
         np.transpose(
-            vutils.make_grid(fake, padding=2, normalize=True, scale_each=True).cpu(),
+            vutils.make_grid(
+                fake, nrow=4, padding=2, normalize=True, scale_each=True
+            ).cpu(),
             (1, 2, 0),
         ),
     )
