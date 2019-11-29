f739e0c60eff8cf45d3797f0a8a932ac44a7dce7
YwuanChai <itsmystyle.0821@gmail.com>
2019-11-29 11:14:13 +0800

    added xavier weight init

--- module/utils.py
+++ module/utils.py
@@ -14,10 +14,19 @@ def set_random_seed(seed):
     # torch.backends.cudnn.benchmark = False
 
 
-def weights_init(m):
+def weights_init(m, mode="xavier"):
     classname = m.__class__.__name__
-    if classname.find("Conv") != -1:
-        nn.init.normal_(m.weight.data, 0.0, 0.02)
-    elif classname.find("BatchNorm") != -1:
-        nn.init.normal_(m.weight.data, 1.0, 0.02)
-        nn.init.constant_(m.bias.data, 0)
+
+    if mode == "normal":
+        if classname.find("Conv") != -1:
+            nn.init.xavier_uniform_(m.weight)
+        elif classname.find("BatchNorm") != -1:
+            nn.init.normal_(m.weight, 1.0, 0.02)
+            nn.init.constant_(m.bias, 0)
+
+    else:
+        if classname.find("Conv") != -1:
+            nn.init.normal_(m.weight.data, 0.0, 0.02)
+        elif classname.find("BatchNorm") != -1:
+            nn.init.normal_(m.weight.data, 1.0, 0.02)
+            nn.init.constant_(m.bias.data, 0)
