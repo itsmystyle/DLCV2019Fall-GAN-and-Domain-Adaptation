499eb4bce453904a76ed94637333a906c7b79610
YwuanChai <itsmystyle.0821@gmail.com>
2019-12-01 17:26:28 +0800

    added resnet18,  changed paper model architecture

--- module/dsn/dsn.py
+++ module/dsn/dsn.py
@@ -1,79 +1,80 @@
 import torch.nn as nn
-import torch.nn.functional as F
+import torchvision.models as models
 
 from module.dsn.utils import Flatten
 from module.da.utils import GradientReversalLayer
 
 
 class DSN(nn.Module):
-    def __init__(self, feature_dim=32, latent_dim=100, p=0.5):
+    def __init__(self, feature_dim=64, latent_dim=512, p=0.5):
         super(DSN, self).__init__()
 
         self.latent_dim = latent_dim
         self.feature_dim = feature_dim
         self.p = p
 
-        def build_feature_extractor(feature_dim, latent_dim):
-            return nn.Sequential(
-                nn.Conv2d(3, feature_dim, 5),
-                nn.BatchNorm2d(feature_dim),
-                nn.MaxPool2d(2),
-                nn.ReLU(True),
-                nn.Conv2d(feature_dim, feature_dim * 2, 5),
-                nn.BatchNorm2d(feature_dim * 2),
-                nn.ReLU(True),
-                nn.Dropout2d(self.p),
-                nn.MaxPool2d(2),
-                Flatten(),
-                nn.Linear(feature_dim * 2 * 4 * 4, latent_dim),
-            )
+        backboneA = models.resnet18(pretrained=True)
+        self.source_encoder = nn.Sequential(
+            *(list(backboneA.children())[:-3]),
+            Flatten(),
+            nn.Linear(1024, self.latent_dim),
+            nn.ReLU(True),
+        )
 
-        # encoders
-        self.source_encoder = build_feature_extractor(self.feature_dim, self.latent_dim)
+        backboneB = models.resnet18(pretrained=True)
+        self.target_encoder = nn.Sequential(
+            *(list(backboneB.children())[:-3]),
+            Flatten(),
+            nn.Linear(1024, self.latent_dim),
+            nn.ReLU(True),
+        )
 
-        self.target_encoder = build_feature_extractor(self.feature_dim, self.latent_dim)
-
-        self.shared_encoder = build_feature_extractor(self.feature_dim, self.latent_dim)
+        backboneC = models.resnet18(pretrained=True)
+        self.shared_encoder = nn.Sequential(
+            *(list(backboneC.children())[:-3]),
+            Flatten(),
+            nn.Linear(1024, self.latent_dim),
+            nn.ReLU(True),
+        )
 
         # decoders
         self.shared_decoder_linear = nn.Sequential(
-            nn.Linear(self.latent_dim, self.feature_dim * 2 * 4 * 4),
-            nn.BatchNorm1d(self.feature_dim * 2 * 4 * 4),
+            nn.Linear(self.latent_dim, self.feature_dim * 2 * 10 * 10),
+            nn.BatchNorm1d(self.feature_dim * 2 * 10 * 10),
             nn.ReLU(True),
-            nn.Dropout(self.p),
         )
         self.shared_decoder = nn.Sequential(
-            nn.ConvTranspose2d(self.feature_dim * 2, self.feature_dim, 5, 2, 1),
+            nn.ConvTranspose2d(self.feature_dim * 2, self.feature_dim, 4, 2, 1),
+            nn.BatchNorm2d(self.feature_dim),
+            nn.ReLU(True),
+            nn.ConvTranspose2d(self.feature_dim, self.feature_dim, 4),
+            nn.BatchNorm2d(self.feature_dim),
+            nn.ReLU(True),
+            nn.ConvTranspose2d(self.feature_dim, self.feature_dim, 4),
             nn.BatchNorm2d(self.feature_dim),
             nn.ReLU(True),
             nn.Dropout2d(self.p),
-            nn.ConvTranspose2d(self.feature_dim, self.feature_dim, 5, 2, 1),
-            nn.BatchNorm2d(self.feature_dim),
-            nn.ReLU(True),
-            nn.Dropout2d(self.p),
-            nn.ConvTranspose2d(self.feature_dim, self.feature_dim, 7),
-            nn.BatchNorm2d(self.feature_dim),
-            nn.ReLU(True),
-            nn.Dropout2d(self.p),
-            nn.ConvTranspose2d(self.feature_dim, 3, 4),
+            nn.ConvTranspose2d(self.feature_dim, 3, 3),
         )
 
         # classifiers
         self.shared_encoder_label_predictor = nn.Sequential(
-            nn.Linear(self.latent_dim, 100),
-            nn.BatchNorm1d(100),
+            nn.Linear(self.latent_dim, 1024),
+            nn.BatchNorm1d(1024),
+            nn.ReLU(True),
+            nn.Linear(1024, 1024),
+            nn.BatchNorm1d(1024),
             nn.ReLU(True),
             nn.Dropout(self.p),
-            nn.Linear(100, 10),
+            nn.Linear(1024, 10),
             nn.LogSoftmax(dim=1),
         )
 
         self.shared_encoder_domain_classifier = nn.Sequential(
-            nn.Linear(self.latent_dim, 100),
-            nn.BatchNorm1d(100),
+            nn.Linear(self.latent_dim, 1024),
+            nn.BatchNorm1d(1024),
             nn.ReLU(True),
-            nn.Dropout(self.p),
-            nn.Linear(100, 1),
+            nn.Linear(1024, 1),
             nn.Sigmoid(),
         )
 
@@ -85,19 +86,18 @@ class DSN(nn.Module):
             private_latent = self.target_encoder(X)
 
         shared_latent = self.shared_encoder(X)
-        label = self.shared_encoder_label_predictor(F.relu(shared_latent))
-        reverse_features = GradientReversalLayer.apply(F.relu(shared_latent), Lambda)
+        label = self.shared_encoder_label_predictor(shared_latent)
+        reverse_features = GradientReversalLayer.apply(shared_latent, Lambda)
         domain = self.shared_encoder_domain_classifier(reverse_features)
 
         recon = private_latent + shared_latent
-        recon = F.relu(recon)
         recon = self.shared_decoder_linear(recon)
-        recon = recon.view(-1, self.feature_dim * 2, 4, 4)
+        recon = recon.view(-1, self.feature_dim * 2, 10, 10)
         recon = self.shared_decoder(recon)
 
         return label, domain, shared_latent, private_latent, recon
 
-    def extract_feature(self, X):
+    def feature_extractor(self, X):
         features = self.shared_encoder(X)
 
         return features
