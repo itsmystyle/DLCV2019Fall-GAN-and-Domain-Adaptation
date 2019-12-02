db2cd9b3bd80c326a2a0ed1a51e94dcfbead888d
YwuanChai <itsmystyle.0821@gmail.com>
2019-12-02 19:52:42 +0800

    removed PYTHONPATH

--- hw3_p1p2.sh
+++ hw3_p1p2.sh
@@ -1,3 +1,3 @@
 wget 'https://www.dropbox.com/s/oxld3vcdwxwpqyp/generator_537.pth.tar?dl=1' -O models/dcgan/generator_537.pth.tar
 wget 'https://www.dropbox.com/s/zz6reezhuy4hu3a/generator_603.pth.tar?dl=1' -O models/acgan/generator_603.pth.tar
-PYTHONPATH=`pwd` python3 module/generate_image.py --output_dir $1 --dcgan_model models/dcgan/generator_537.pth.tar --acgan_model models/acgan/generator_603.pth.tar --random_seed 42 821
+python3 module/generate_image.py --output_dir $1 --dcgan_model models/dcgan/generator_537.pth.tar --acgan_model models/acgan/generator_603.pth.tar --random_seed 42 821
