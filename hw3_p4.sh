cc5c514af4dfd18247d0899ef918e00dd624bb2f
YwuanChai <itsmystyle.0821@gmail.com>
2019-12-02 19:56:03 +0800

    changed predict bash

--- hw3_p4.sh
+++ hw3_p4.sh
@@ -1,3 +1,3 @@
 wget 'https://www.dropbox.com/s/p11qsy14bii6qm6/dsn_m2s_model_best.pth.tar?dl=1' -O models/dsn_m2s/model_best.pth.tar
 wget 'https://www.dropbox.com/s/bckqxvktu470bed/dsn_s2m_model_best.pth.tar?dl=1' -O models/dsn_s2m/model_best.pth.tar
-python3 module/dsn/predict.py $1 $2 $3
+python3 predict_dsn.py $1 $2 $3
