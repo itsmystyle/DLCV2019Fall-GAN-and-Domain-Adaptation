0d59e7501be96d591484aafc4cb72077b9f40153
YwuanChai <itsmystyle.0821@gmail.com>
2019-12-02 18:05:45 +0800

    added pythonpath to pwd

--- hw3_p3.sh
+++ hw3_p3.sh
@@ -1,3 +1,3 @@
 wget 'https://www.dropbox.com/s/3cog4s55kp7rlty/dann_m2s_model_best.pth.tar?dl=1' -O models/dann_m2s/model_best.pth.tar
 wget 'https://www.dropbox.com/s/argeu74sx66immu/dann_s2m_model_best.pth.tar?dl=1' -O models/dann_s2m/model_best.pth.tar
-python3 module/da/predict.py $1 $2 $3
+PYTHONPATH=`pwd` python3 module/da/predict.py $1 $2 $3
